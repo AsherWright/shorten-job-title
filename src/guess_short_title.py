54bb2df44cb311e1e9b439f64959f7e36f2cc70e
Asher Wright <sastreen@hotmail.com>
2020-01-05 16:01:22 -0500

    Add basic guesser

---
+++ src/guess_short_title.py
@@ -0,0 +1,52 @@
+import json
+import numpy as np
+import pandas as pd
+from short_title_guesser import ShortTitleGuesser
+from pred_models.simple_rnn import SimpleRnn
+from data_handling.seq_data_formatter import SeqDataFormatter
+
+
+def guess_short_title():
+    model = get_simple_rnn_model()
+    data_formatter = get_seq_data_formatter()
+    long_title = "sales sales head of marketing sales sales"
+
+    stg = ShortTitleGuesser(model, data_formatter)
+    print(stg.guess_short_title(long_title))
+
+
+# TODO: Should instead load an existing model
+def get_simple_rnn_model():
+    X_train = np.load("data/simple_titles_X_train.npy")
+    y_train = np.load("data/simple_titles_y_train.npy")
+
+    simple_rnn = SimpleRnn(50, 10, 0.1, 0.1)
+    simple_rnn.train(X_train, y_train, 100, 50, 0.3)
+
+    return simple_rnn.get_model()
+
+
+# TODO: Shouldn't need to make an entire data formatter here.
+def get_seq_data_formatter():
+    glove_vectors = get_glove_vectors()
+    simple_titles = pd.read_csv("data/simple_titles.csv")
+    output_path = "data/simple_titles"
+    WORDS_PER_LONG_TITLE = 10
+
+    return SeqDataFormatter(
+        simple_titles,
+        glove_vectors,
+        WORDS_PER_LONG_TITLE,
+        output_path
+    )
+
+
+def get_glove_vectors():
+    with open('data/embeddings/glove.json') as f:
+        data = json.load(f)
+
+    return data
+
+
+if __name__ == '__main__':
+    guess_short_title()
---
+++ src/short_title_guesser.py
@@ -0,0 +1,22 @@
+import numpy as np
+from model import Model
+from data_formatter import DataFormatter
+
+
+class ShortTitleGuesser:
+    def __init__(self, model: Model, data_formatter: DataFormatter) -> None:
+        self.model = model
+        self.data_formatter = data_formatter
+
+    def guess_short_title(self, long_title: str) -> str:
+        x = self.data_formatter.format_long_title(long_title)
+        x = np.array(x)
+        x = x.reshape((1, x.shape[0], x.shape[1]))
+
+        preds = np.round(self.model.predict(x))[0]
+        print(preds)
+        words = long_title.split(" ")
+
+        short_title_guess = [w for w, p in zip(words, preds) if p == 1]
+
+        return ' '.join(short_title_guess)
