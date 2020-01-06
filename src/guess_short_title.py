fe8bacdb92e6731a12e393402d9020bce0526658
Asher Wright <sastreen@hotmail.com>
2020-01-05 21:26:14 -0500

    Test harder dataset

--- src/guess_short_title.py
+++ src/guess_short_title.py
@@ -17,8 +17,8 @@ def guess_short_title():
 
 # TODO: Should instead load an existing model
 def get_simple_rnn_model():
-    X_train = np.load("data/easy_titles_X_train.npy")
-    y_train = np.load("data/easy_titles_y_train.npy")
+    X_train = np.load("data/med_titles_X_train.npy")
+    y_train = np.load("data/med_titles_y_train.npy")
 
     simple_rnn = SimpleRnn(50, 10, 0.1, 0.1)
     simple_rnn.train(X_train, y_train, 100, 50, 0.3)
@@ -29,12 +29,12 @@ def get_simple_rnn_model():
 # TODO: Shouldn't need to make an entire data formatter here.
 def get_seq_data_formatter():
     glove_vectors = get_glove_vectors()
-    easy_titles = pd.read_csv("data/easy_titles.csv")
-    output_path = "data/easy_titles"
+    med_titles = pd.read_csv("data/med_titles.csv")
+    output_path = "data/med_titles"
     WORDS_PER_LONG_TITLE = 10
 
     return SeqDataFormatter(
-        easy_titles,
+        med_titles,
         glove_vectors,
         WORDS_PER_LONG_TITLE,
         output_path
