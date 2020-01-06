d1a6da98ffaaf8d44529931a5b59c8096947d53d
Asher Wright <sastreen@hotmail.com>
2020-01-05 21:23:17 -0500

    Remove redundant data helpers

---
+++ src/data_handling/med_data_generator.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+
+
+# TODO: Should probably be merged or share code with easy
+class MedDataGenerator:
+    def __init__(self, save_path):
+        self.save_path = save_path
+
+    def generate_med_data(self):
+        short_titles = []
+        long_titles = []
+        short_title_indices = []
+
+        for pre_padding in range(5):
+            for post_padding in range(5):
+                for _i in range(100):
+                    lt, st, sti = get_long_short_titles(
+                        pre_padding,
+                        post_padding
+                    )
+                    long_titles.append(lt)
+                    short_titles.append(st)
+                    short_title_indices.append(sti)
+
+        self.long_titles = long_titles
+        self.short_titles = short_titles
+        self.short_title_indices = short_title_indices
+
+    def save_data(self):
+        data = {
+            'long_title': self.long_titles,
+            'short_title': self.short_titles,
+            'short_title_index': self.short_title_indices
+        }
+
+        pd.DataFrame(data).to_csv(self.save_path, index=False)
+
+
+def get_long_short_titles(pre_padding, post_padding):
+    words = [
+        "marketing",
+        "sales",
+        "ceo",
+        "chief",
+        "finance",
+        "north",
+    ]
+
+    short_title = np.random.choice(words)
+    prefix = get_prefix(pre_padding, words)
+    suffix = get_suffix(post_padding, words)
+
+    long_title = prefix + "head of " + short_title + suffix
+    short_title_index = pre_padding + 2
+
+    return long_title, short_title, short_title_index
+
+
+def get_prefix(pre_padding, words):
+    prefix = ""
+
+    for i in range(pre_padding):
+        prefix += np.random.choice(words) + " "
+
+    return prefix
+
+
+def get_suffix(post_padding, words):
+    if post_padding == 0:
+        return ""
+
+    suffix = ""
+
+    for i in range(post_padding):
+        suffix += " " + np.random.choice(words)
+
+    return suffix
--- src/handle_data.py
+++ src/handle_data.py
@@ -1,12 +1,62 @@
-from data_handling.generate_raw_data import generate_raw_data
-from data_handling.sequentially_format_data import sequentially_format_data
-from data_handling.split_data import split_data
+import pandas as pd
+import json
+from data_handling.easy_data_generator import EasyDataGenerator
+from data_handling.med_data_generator import MedDataGenerator
+from data_handling.seq_data_formatter import SeqDataFormatter
+from data_handling.data_splitter import DataSplitter
 
 
+# TODO: this needs to be cleaned up a lot
 def handle_data():
     generate_raw_data()
-    sequentially_format_data()
-    split_data()
+    sequentially_format_data("data/easy_titles.csv", "data/easy_titles")
+    sequentially_format_data("data/med_titles.csv", "data/med_titles")
+    split_data("data/easy_titles_X", "data/easy_titles_y", ".npy")
+    split_data("data/med_titles_X", "data/med_titles_y", ".npy")
+
+
+def generate_raw_data():
+    easy_data_file = "data/easy_titles.csv"
+    med_data_file = "data/med_titles.csv"
+
+    easy_dg = EasyDataGenerator(easy_data_file)
+    med_dg = MedDataGenerator(med_data_file)
+
+    easy_dg.generate_easy_data()
+    med_dg.generate_med_data()
+
+    easy_dg.save_data()
+    med_dg.save_data()
+
+
+def sequentially_format_data(input_path, output_path):
+    glove_vectors = get_glove_vectors()
+    easy_titles = pd.read_csv(input_path)
+    WORDS_PER_LONG_TITLE = 10
+
+    seq_data_formatter = SeqDataFormatter(
+        easy_titles,
+        glove_vectors,
+        WORDS_PER_LONG_TITLE,
+        output_path
+    )
+
+    seq_data_formatter.format_data()
+    seq_data_formatter.save_data()
+
+
+def get_glove_vectors():
+    with open('data/embeddings/glove.json') as f:
+        data = json.load(f)
+
+    return data
+
+
+def split_data(x_file, y_file, ext):
+    data_splitter = DataSplitter(x_file, y_file, ext, 0.3)
+
+    data_splitter.split_data()
+    data_splitter.save_data()
 
 
 if __name__ == '__main__':
